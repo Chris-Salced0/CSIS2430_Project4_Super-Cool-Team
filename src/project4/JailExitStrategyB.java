79219b82093fcdd15da3d5af0dd6a75a540f8bae
taylerkaye <tayler.covington@gmail.com>
2026-05-03 18:24:04 -0600

    Update JailExitStrategyB.java
    Saw what you did for A and just copied and pasted to B with minor applicable changes.

--- src/project4/JailExitStrategyB.java
+++ src/project4/JailExitStrategyB.java
@@ -1,5 +1,40 @@
+/*
+ * Team Name:    SuperCoolTeam
+ * Team Members: Tayler Covington-Holler (Communication Lead)
+ *               Cameron Dickinson (Implementation Lead)
+ *               Christopher Salcedo Cardenas (Verification Lead)
+ *               Maxwell Jardine (Implementation Lead)
+ * Course:       CS 2430-501
+ * Project:      Project 4 – Capstone: Monopoly Simulation
+ * Primary Author: Maxwell Jardine
+ */
 package project4;
 
+/**
+ * Represents Jail Strategy B.
+ *
+ * If the player does not have a Get Out Of Jail Free card, the player attempts 
+ * to roll doubles for up to 3 turns. If unsuccessful after 3 attempts, the 
+ * simulation assumes the player pays $50 and leaves Jail on the 4th turn.
+ */
 public class JailExitStrategyB {
 
+    /**
+     * Returns jail strategy name.
+     *
+     * @return Strategy B name
+     */
+    public String getName() {
+        return "Strategy B - Try for Doubles";
+    }
+
+    /**
+     * Tells the TurnEngine whether this strategy tries to roll doubles before
+     * leaving Jail.
+     *
+     * @return true because Strategy B attempts to roll doubles
+     */
+    public boolean triesForDoubles() {
+        return true;
+    }
 }
